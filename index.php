80f3371887b23457108c933c241f840921871910
cjordanOU <cjordan@oakland.edu>
2021-10-08 01:18:03 -0400

    Added initial index framework

--- index.php
+++ index.php
@@ -10,6 +10,24 @@
         <link href="Styles/style.css" rel="stylesheet">
     </head>
     <body>
-        
+        <section id="main-page">
+            <header>
+                <div class="header-top">
+                    <a href="index.php" title="logo" class="header-logo">LOGO</a>
+                    <input class="header-search" type="search" placeholder="Search..">
+                    <a href="login.php" title="Click here to log in" class="header-login button-1">Login</a>
+                    <a href="signup.php" title="Click here to sign up" class="header-signup button-1 button-dark">Sign Up</a>
+                </div>
+                <div class="header-bottom">
+                    <nav class="header-container">
+                        <a href="#" title="Placeholder Header Link">Link #1</a>
+                        <a href="#" title="Placeholder Header Link">Link #2</a>
+                        <a href="#" title="Placeholder Header Link">Link #3</a>
+                        <a href="#" title="Placeholder Header Link">Link #4</a>
+                        <a href="#" title="Placeholder Header Link">Link #5</a>
+                    </nav>
+                </div>
+            </header>
+        </section>
     </body>
 </html>
