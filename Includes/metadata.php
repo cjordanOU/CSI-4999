a2bec9ca1fcb34953aecc55ab39c2f7723fba4a0
Cam <cjordan@oakland.edu>
2021-10-07 22:20:43 -0400

    Add files via upload

---
+++ Includes/metadata.php
@@ -0,0 +1,11 @@
+<?php
+    echo '<meta charset="utf-8">';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+    echo '<meta name="description" content="ENTER DESCRIPTION HERE">';
+    echo '<meta name="keywords" content="KEYWORDS, GO, HERE">';
+    echo '<meta name="author" content="AUTHOR, NAMES, HERE">';
+    echo '<!-- Linked Fonts -->';
+    echo '<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">';
+    echo '<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">';
+    echo '<!-- Title -->';
+    ?>
