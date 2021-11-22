7b7503fa1ee3ef1690628423a8626fd1404ff44e
cjordanOU <cjordan@oakland.edu>
2021-11-21 20:59:42 -0500

    profile bugfixes

--- Includes/profileHandler.php
+++ Includes/profileHandler.php
@@ -5,15 +5,15 @@
 
     function displayUsersPosts() {
         // Select all of the user's posts and display them
-        $sql = "SELECT * FROM posts WHERE User_Info_USER_ID={$_SESSION["id"]}";
-        //$sql = "SELECT * FROM posts WHERE User_Info_USER_ID=1"; // DEBUG HARD CODE
+        $currentID = $_SESSION["id"];
+        $sql = "SELECT * FROM posts INNER JOIN threads on posts.PARENT_THREAD = threads.THREADS_ID WHERE posts.User_Info_USER_ID={$_SESSION["id"]}";        //$sql = "SELECT * FROM posts WHERE User_Info_USER_ID={$_SESSION["id"]}";
         $dbConnection = $GLOBALS['dbConnection']; // Important since this code is inside a function
         $result = $dbConnection-> query($sql);
 
         if ($result-> num_rows > 0) {
             while ($row = $result-> fetch_assoc()) {
                 echo "<section class='post-box'>";
-                echo "<div class='post-box-title'><h3 title='". $row["POST_TIME"] ."'>". $row["PARENT_THREAD"] ."</h3><hr></div>";
+                echo "<div class='post-box-title'><h3 title='". $row["POST_TIME"] ."'>". $row["THREAD_TITLE"] ."</h3><hr></div>";
                 echo "<div class='post-box-content'><p>". $row["CONTENT"] ."</p></div>";
                 echo "</section>";
             }
