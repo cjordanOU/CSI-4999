22aa3adbc6982aee37863f39579dc181c2293751
Theo <72151761+jmoseleyou@users.noreply.github.com>
2021-12-07 17:08:34 -0800

    Update about.php

--- about.php
+++ about.php
@@ -24,8 +24,36 @@
         <section id="page-content">
             
             <section id="page-container">
+
+                <section id="slideshowContainer">
+                    <div id="slideshowImage" class="parallaxBG-large">
+                        <div id="slideshowFade"></div>
+                    </div>
+                    <div class="parallaxText1">
+                        <h2>About Us</h2>
+                    </div>
+                </section>
                 
+                <main>
+                    <section>
+                    <h1>What is GrizzChat?</h1>
+                         <p>GrizzChat was an idea inspired to help bridge communication between Oakland University students
+						 as well as Oakland University Alumni. One of the many reasons why this application was created was 
+						 that Oakland University students did not have a platform for them to reach out to each other. Futhremore,
+						  there are not many outlets for alumni to reach out to current students. GrizzChat is an online forum where 
+						  students and alumni to reach out and talk to one anoter.
+                         </p>
+						 <br><br><br>
+					<h1> Our Vision</h1>
+						<p> Our vision is to create a platform that will evolve and help better connect students and 
+						alumni. We hope that this can become a communication tool for all people involved with Oakland University. 
+						The end-goal of GrizzChat is to become a central communication tool for all of Oakland University.
+						</p>
+						<br><br><br>
+
+                </main>
             </section>
+            
 
             <?php displayFooter(); ?>
         </section>
